8beb4499baa3708b0d080b5746b1e96874d7f8bc
macy11 <macy11@lenovo.com>
2019-09-06 17:51:27 +0800

    create project

---
+++ app/src/main/java/com/mcy/define/customview/SpiderView.java
@@ -0,0 +1,31 @@
+package com.mcy.define.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @ProjectName: DefineStudyDemo
+ * @Package: com.mcy.define.customview
+ * @ClassName: SpiderView
+ * @Description: java类作用描述
+ * @Author: macy
+ * @CreateDate: 2019/9/4 11:23
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/9/4 11:23
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class SpiderView extends View {
+    public SpiderView(Context context) {
+        super(context);
+    }
+
+    public SpiderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SpiderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
