c166f976e60c0bc000c20bb20bf6ea292406c040
macy11 <macy11@lenovo.com>
2019-09-13 17:55:12 +0800

    xxxxxxxx

--- app/src/main/java/com/mcy/define/MainActivity.java
+++ app/src/main/java/com/mcy/define/MainActivity.java
@@ -14,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
+=======
+
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     }
 }
