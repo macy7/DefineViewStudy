7ac98cbff581ff21eb6b824d2cbae3eb2f8671cd
macy11 <macy11@lenovo.com>
2019-09-13 17:35:33 +0800

    add new file with network other

--- app/src/main/java/com/mcy/define/MainActivity.java
+++ app/src/main/java/com/mcy/define/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mcy.define;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
